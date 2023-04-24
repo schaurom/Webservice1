9a43e7f713352a97f8f1be5048cba64036cef23b
schaurom <118014904+schaurom@users.noreply.github.com>
2023-04-24 22:02:12 +0200

    zweite Änderung

--- main.py
+++ main.py
@@ -3,14 +3,24 @@
 # Press Umschalt+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+from flask import Flask, request
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+app = Flask(__name__)
 
+@app.route('/multiply')
+def multiply():
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+    result = a * b
+    return str(result)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm sdfsdfsdf')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# def print_hi(name):
+#     # Use a breakpoint in the code line below to debug your script.
+#     print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+#
+#
+# # Press the green button in the gutter to run the script.
+# if __name__ == '__main__':
+#     print_hi('PyCharm sdfsdf')
+#
+# # See PyCharm help at https://www.jetbrains.com/help/pycharm/
