5559ba4e7e5ec82baff7e7617674ffaf99b83658
schaurom <118014904+schaurom@users.noreply.github.com>
2023-04-24 22:21:39 +0200

    Fehlerbehandlung

--- your_python_file.py
+++ your_python_file.py
@@ -5,8 +5,18 @@ print('Hallo')
 
 @app.route('/multiply')
 def multiply():
-    a = int(request.args.get('a'))
-    b = int(request.args.get('b'))
+    a = request.args.get('a')
+    b = request.args.get('b')
+
+    if a is None or b is None:
+        return 'Error: a an b are required parameters.'
+
+    try:
+        a=int(a)
+        b=int(b)
+    except ValueError:
+        return 'Error: a and b must be integers.'
+
     result = a * b
     return str(result)
 
