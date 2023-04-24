6fc2fecad54729be7cd330399a3ac2e4828fc0bf
schaurom <118014904+schaurom@users.noreply.github.com>
2023-04-24 22:09:05 +0200

    app.run() eingefügt

--- your_python_file.py
+++ your_python_file.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 app = Flask(__name__)
-
+print('Hallo')
 
 @app.route('/multiply')
 def multiply():
@@ -9,3 +9,6 @@ def multiply():
     b = int(request.args.get('b'))
     result = a * b
     return str(result)
+
+if __name__ == '__main__':
+    app.run()
